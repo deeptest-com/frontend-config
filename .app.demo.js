fd66ebf1b1bb6b126add2c94db71105d97c4d013
chenqi99 <chenqi99@nancal.com>
2023-12-05 10:43:08 +0800

    update

--- .app.demo.js
+++ .app.demo.js
@@ -1,4 +1,4 @@
 module.exports = {
-    VUE_APP_API_SERVER: 'http://124.221.203.74:8085/api/v1',
-    VUE_APP_API_AGENT: 'http://124.221.203.74:8086/api/v1'
+    VUE_APP_API_SERVER: 'http://server.api.deeptest.com/api/v1',
+    VUE_APP_API_AGENT: 'http://agent.api.deeptest.com/api/v1'
 }
